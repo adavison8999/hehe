c7a149a2c6206ddf2525bf82af6ce5aef16881c8
adavison8999 <57542656+adavison8999@users.noreply.github.com>
2019-11-08 14:28:43 -0500

    Add files via upload

---
+++ AdventureGame.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour
+{
+    [SerializeField] Text textCompnent;
+    // available now within inspector
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+        textCompnent.text = "Hello there sir";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
